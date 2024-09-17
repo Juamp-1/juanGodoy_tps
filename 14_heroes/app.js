22874da5dbc4f4bf9064e15c8e51110bc535e01f
Juamp-1 <juanpgodoy04@gmail.com>
2024-09-17 00:12:17 -0300

    css add

--- 14_heroes/app.js
+++ 14_heroes/app.js
@@ -28,4 +28,24 @@ app.get('/berners-lee', (req,res) => {
     return res.sendFile(path.join(__dirname, 'views', 'berners-lee.html'))
 });
 
+app.get('/clarke', (req,res) => {
+    return res.sendFile(path.join(__dirname, 'views', 'clarke.html'))
+});
+
+app.get('/hamilton', (req,res) => {
+    return res.sendFile(path.join(__dirname, 'views', 'hamilton.html'))
+});
+
+app.get('/hopper', (req,res) => {
+    return res.sendFile(path.join(__dirname, 'views', 'hopper.html'))
+});
+
+app.get('/lovelace', (req,res) => {
+    return res.sendFile(path.join(__dirname, 'views', 'lovelace.html'))
+});
+
+app.get('/turing', (req,res) => {
+    return res.sendFile(path.join(__dirname, 'views', 'turing.html'))
+});
+
 app.listen(PORT, () => console.log('Server running in http://localhost:' + PORT))
