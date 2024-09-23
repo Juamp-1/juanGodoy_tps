194db3fc998df2eaf18a6b81d87c37eeb612bd58
Juamp-1 <juanpgodoy04@gmail.com>
2024-09-23 19:39:16 -0300

    TP 11 finished

--- 16_portfolio/app.js
+++ 16_portfolio/app.js
@@ -8,6 +8,7 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', (req, res) => { 
     return res.sendFile(path.join(__dirname,'views', 'home.html'))
-});
+    });
 
 app.listen(PORT, () => console.log("Server running in http://localhost:" + PORT));
+
